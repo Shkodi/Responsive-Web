45dd2e4bb5cc03905631848b8e35b7748785a721
Shkodi <sh40385@universum-ks.org>
2023-05-05 12:56:01 +0200

    commit

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+if(window.innerWidth < 1200) {
+    let icon = document.querySelector("header i");
+    let nav = document.querySelector("nav");
+    let links = document.querySelectorAll("nav li");
+
+    function toggleNav() {
+        if(window.getComputedStyle(nav).display == "none") {
+            icon.classList.replace("fa-bars", "fa-times");
+            nav.style.display = "block";
+        }
+        else {
+            icon.classList.replace("fa-times", "fa-bars");
+            nav.style.display = "none";
+        }
+    }
+
+    icon.addEventListener("click", toggleNav);
+    links.forEach(item => item.addEventListener("click", toggleNav));
+}
