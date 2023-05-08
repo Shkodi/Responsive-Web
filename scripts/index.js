5ca1e09cb87b6fcd98c76585db14bd231882a338
Shkodi <77227480+Shkodi@users.noreply.github.com>
2023-05-08 08:52:47 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,21 @@
-if(window.innerWidth < 1200) {
-    let icon = document.querySelector("header i");
-    let nav = document.querySelector("nav");
-    let links = document.querySelectorAll("nav li");
+document.addEventListener("resize", function() {  
+    if(window.innerWidth < 1200) {
+        let icon = document.querySelector("header i");
+        let nav = document.querySelector("nav");
+        let links = document.querySelectorAll("nav li");
 
-    function toggleNav() {
-        if(window.getComputedStyle(nav).display == "none") {
-            icon.classList.replace("fa-bars", "fa-times");
-            nav.style.display = "block";
+        function toggleNav() {
+            if(window.getComputedStyle(nav).display == "none") {
+                icon.classList.replace("fa-bars", "fa-times");
+                nav.style.display = "block";
+            }
+            else {
+                icon.classList.replace("fa-times", "fa-bars");
+                nav.style.display = "none";
+            }
         }
-        else {
-            icon.classList.replace("fa-times", "fa-bars");
-            nav.style.display = "none";
-        }
+
+        icon.addEventListener("click", toggleNav);
+        links.forEach(item => item.addEventListener("click", toggleNav));
     }
-
-    icon.addEventListener("click", toggleNav);
-    links.forEach(item => item.addEventListener("click", toggleNav));
-}
+});  
